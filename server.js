1405b1d0081f9390e155203a164d1ef914369940
unknown <chandler.baird21@gmail.com>
2022-12-04 09:02:58 -0700

    added boiler plate for server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// Import the connection object
+
+// custom module
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to the database before starting the Express.js server
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
